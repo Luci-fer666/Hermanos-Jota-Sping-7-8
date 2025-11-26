49006bf91e10a11ff24918a6d90dc1583514e436
Luci-fer666 <luciano.illuminati17@gmail.com>
2025-11-26 14:36:01 -0300

    Intento de rutas de creacion de usuarios en backend

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../usuarios');
+const Usuario = require('./DB/models/User');
+const bcrypt = require('bcrypt');
+
+
+//Logica de Usuarios
+router.post('/register', async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser) {
+      return res.status(400).json({ message: 'El email o nombre de usuario ya está en uso.' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+
+    const nuevoUsuario = new Usuario({
+      username,
+      email,
+      password: hashedPassword,}
+    );
+
+    const usuarioGuardado = await nuevoUsuario.save();
+    res.status(201).json({message: 'Usuario creado', usuario: usuarioGuardado});
+
+  } catch (error) {
+    error.status = 400;
+    next(error);
+  }
+});
+
 
 // GET /users → devuelve todos los usuarios
 router.get('/', (req, res) => {
