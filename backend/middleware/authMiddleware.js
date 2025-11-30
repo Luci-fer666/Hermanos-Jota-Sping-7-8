02d2564142f33b29dff207a3d2504703eafbd0ab
Luci-fer666 <luciano.illuminati17@gmail.com>
2025-11-30 02:59:14 -0300

    Correcciones en el front. Logica de pedidos incompleta

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,19 +3,27 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'No autorizado: Debes iniciar sesión (Falta Token)' });
-    }
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        // Estraer el token del encabezado
+        const token = authHeader.split(' ')[1];
 
-    const token = authHeader.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({message: "No autorizado."});
+        }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;                 // guardamos los datos del usuario
-        next();                             // seguimos a la ruta protegida
-    } catch (error) {
-        return res.status(403).json({ message: 'Error: prohibido.' });
+        const decodedPayload = jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+            if (err) {
+                return res.status(403).json({message: "Error: prohibido."});
+            }
+
+            // metemos los datos del usuario en la petición req
+            req.user = decodedPayload;
+
+            next();
+        });
+    } else {
+        res.status(401).json({message: 'No autorizado: Debes iniciar sesión (Falta Token)'});
     }
-};
+}
 
 module.exports = verifyToken;
